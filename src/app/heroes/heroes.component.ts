63c608f7c902353140440c8f5f8f5437750b3bd1
eric <eathom91@gmail.com>
2022-07-06 14:24:59 -0400

    Implemented heroes-component to display/edit a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Hero} from "../hero";
+// Component decorator, imported from @angular/core libra
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+// Always export class component so it can be used elsewhere...
+// like in the AppModule.
 export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
